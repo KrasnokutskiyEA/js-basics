ded577276d150c0ee52b8e3140e737cdf76a0296
krasnokutskiy.e <Krasnokutskiy.E@ostec-group.ru>
2021-07-04 04:08:07 +0300

    WIP

--- callapplybind.js
+++ callapplybind.js
@@ -1,3 +1,4 @@
+// контекст - область видимости + ссылка на объект который вызывает код в данный момент
 const animal = {
   hasTail: true,
   hasLegs: true
@@ -7,7 +8,7 @@ const f1 = function (arg1, arg2, arg3) {
   console.log(this, arg1 + arg2 + arg3)
 }
 
-f1('A', 'B', 'C') // Window, 'ABC'
+// f1('A', 'B', 'C') // Window, 'ABC'
 
 // 1 - bind возвращает новый экземпляр ф-ии c привязанным контекстом:
 const f2 = f1.bind(animal, 'A', 'B', 'C')
@@ -18,3 +19,7 @@ f1.call(animal, 'A', 'B', 'C') // animal object, 'ABC'
 
 // 3 - apply - как call, но аргументы в массиве:
 f1.apply(animal, ['A', 'B', 'C']) // animal object, 'ABC'
+
+// 4 - стрелочная ф-ия
+const fArrow = () => console.log(this)
+fArrow.call(animal) // Window !!!. стр.фия не имеет this !!!
