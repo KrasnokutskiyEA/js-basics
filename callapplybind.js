cf7e6903eb89b86cc3a35c08b6feb365ff391592
krasnokutskiy.e <Krasnokutskiy.E@ostec-group.ru>
2021-07-04 03:42:35 +0300

    WIP

--- callapplybind.js
+++ callapplybind.js
@@ -1,1 +1,20 @@
-console.log('AAAA')
+const animal = {
+  hasTail: true,
+  hasLegs: true
+}
+
+const f1 = function (arg1, arg2, arg3) {
+  console.log(this, arg1 + arg2 + arg3)
+}
+
+f1('A', 'B', 'C') // Window, 'ABC'
+
+// 1 - bind возвращает новый экземпляр ф-ии c привязанным контекстом:
+const f2 = f1.bind(animal, 'A', 'B', 'C')
+f2() // animal object, 'ABC'
+
+// 2 - call - контекст создается в момент запуска ф-ии:
+f1.call(animal, 'A', 'B', 'C') // animal object, 'ABC'
+
+// 3 - apply - как call, но аргументы в массиве:
+f1.apply(animal, ['A', 'B', 'C']) // animal object, 'ABC'
